72d79039b834e761bd1c287ae06603445e66d1f2
John W Deng <john.w.deng@pwc.com>
2017-12-19 13:01:53 +0800

    第一次提交博客工程

---
+++ john-deng-blog/src/main/java/john/w/deng/blog/john_deng_blog/App.java
@@ -0,0 +1,13 @@
+package john.w.deng.blog.john_deng_blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
